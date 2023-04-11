c4c26b196346451bbeb489e3dcd55069b65672d3
Ni Luh Nyoman Ayu Menuh <127836672+ayu2201010551@users.noreply.github.com>
2023-04-11 16:54:29 +0800

    uploud project

---
+++ konfigurasi.php
@@ -0,0 +1,9 @@
+<?php
+//konfigurasi koneksi DBMS ke mysql
+     define("DBHOST", "localhost");
+     define("DBUSER", "root");
+     define("DBPASS", "");
+     define("DBPORT", "3306");
+     define("DBNAME", "dbmahasiswa");
+
+
